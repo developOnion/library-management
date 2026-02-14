ba1893abbb3a82bf1e67e7b5d0f741a3634f2532
developOnion <smeyonion.nou@gmail.com>
2026-02-14 23:04:44 +0700

    Add User.java class

--- src/main/java/com/oop/library_management/domain/model/Role.java
+++ src/main/java/com/oop/library_management/domain/model/Role.java
@@ -32,6 +32,6 @@ public class Role {
 		this.name = name;
 	}
 
-
+	
 
 }
--- src/main/java/com/oop/library_management/domain/model/User.java
+++ src/main/java/com/oop/library_management/domain/model/User.java
@@ -1,7 +1,118 @@
 package com.oop.library_management.domain.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 
-public class User {
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(name = "first_name", nullable = false, length = 100)
+	private String firstName;
+
+	@Column(name = "last_name" ,nullable = false, length = 100)
+	private String lastName;
+
+	@Column(nullable = false,	unique = true ,length = 150)
+	private String email;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "role_id", nullable = false)
+	private Role role;
+
+	@Column(name = "last_login")
+	private LocalDateTime lastLogin;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	public User() {
+		this.createdAt = LocalDateTime.now();
+	}
+
+	public User(
+			String username,
+			String password,
+			String firstName,
+			String lastName,
+			String email,
+			Role role
+	) {
+
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.role = role;
+		this.createdAt = LocalDateTime.now();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(LocalDateTime lastLogin) {
+		this.lastLogin = lastLogin;
+	}
 }
