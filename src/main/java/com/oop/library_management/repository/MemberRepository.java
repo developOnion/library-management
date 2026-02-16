c67301f709ab350b63e6cec1e375da66b96ea40e
developOnion <smeyonion.nou@gmail.com>
2026-02-16 20:59:39 +0700

    add: MemberRepository.java

---
+++ src/main/java/com/oop/library_management/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.oop.library_management.repository;
+
+import com.oop.library_management.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
