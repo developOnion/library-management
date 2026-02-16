451e34e7920710b560b3a0d021bcd0f12f8a8c01
developOnion <smeyonion.nou@gmail.com>
2026-02-16 12:37:29 +0700

    migrate to spring boot

--- src/main/java/com/oop/library_management/Application.java
+++ src/main/java/com/oop/library_management/Application.java
@@ -1,19 +1,13 @@
 package com.oop.library_management;
 
-import com.oop.library_management.config.HibernateUtil;
-import org.hibernate.Session;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-
-		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-			System.out.println("Hibernate session opened successfully!");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			HibernateUtil.shutdown();
-		}
+		SpringApplication.run(Application.class, args);
 	}
 
 }
