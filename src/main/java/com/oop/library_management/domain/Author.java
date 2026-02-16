c5da1afddd9fcb730244ef12f2289fd611f78f66
developOnion <smeyonion.nou@gmail.com>
2026-02-16 10:26:35 +0700

    add properties validation for Author.java

--- src/main/java/com/oop/library_management/domain/Author.java
+++ src/main/java/com/oop/library_management/domain/Author.java
@@ -1,6 +1,9 @@
 package com.oop.library_management.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "authors")
@@ -10,10 +13,14 @@ public class Author {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "first_name", nullable = false, length = 100)
+	@Column(name = "first_name", nullable = false, length = 50)
+	@NotBlank(message = "First name is required")
+	@Size(max = 50, message = "First name must be at most 50 characters")
 	private String firstName;
 
-	@Column(name = "last_name", nullable = false, length = 100)
+	@Column(name = "last_name", nullable = false, length = 50)
+	@NotBlank(message = "Last name is required")
+	@Size(max = 50, message = "Last name must be at most 50 characters")
 	private String lastName;
 
 	protected Author() {
