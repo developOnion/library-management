ee7c90bb2ec0f297655445a1e6339b5f58ca5964
developOnion <smeyonion.nou@gmail.com>
2026-02-24 15:27:48 +0700

    change application.properties to application.yml

--- src/main/java/com/oop/library_management/security/CustomUserDetailsService.java
+++ src/main/java/com/oop/library_management/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.oop.library_management.security;
 
 import com.oop.library_management.model.user.User;
 import com.oop.library_management.repository.UserRepository;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
